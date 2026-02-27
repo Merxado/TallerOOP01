ca2c06340e049bf987903b64f297cee259075fcf
Merxado <mayimamiyyo1112@gmail.com>
2026-02-27 13:22:56 -0500

    Add Properties and corrections

--- OOP01/01Bakends/Time.cs
+++ OOP01/01Bakends/Time.cs
@@ -8,6 +8,26 @@
         private int _seconds;
         private int _milliseconds;
 
+        public int Hour
+        {
+            get => _hours;
+        }
+
+        public int Minute
+        {
+            get => _minutes;
+        }
+
+        public int Second
+        {
+            get => _seconds;
+        }
+
+        public int Millisecond
+        {
+            get => _milliseconds;
+        }
+
         // Constructors
         public Time() : this(0, 0, 0, 0)
         {
@@ -24,20 +44,36 @@
         public Time(int hours, int minutes, int seconds) : this(hours, minutes, seconds, 0)
         {
         }
+        private void ValidHour(int hour)
+        {
+            if (hour < 0 || hour > 23)
+                throw new ArgumentException($"The hour: {hour}, is not valid.");
+        }
+
+        private void ValidMinute(int minute)
+        {
+            if (minute < 0 || minute > 59)
+                throw new ArgumentException($"The minute: {minute}, is not valid.");
+        }
+
+        private void ValidSecond(int second)
+        {
+            if (second < 0 || second > 59)
+                throw new ArgumentException($"The second: {second}, is not valid.");
+        }
+
+        private void ValidMillisecond(int millisecond)
+        {
+            if (millisecond < 0 || millisecond > 999)
+                throw new ArgumentException($"The millisecond: {millisecond}, is not valid.");
+        }
 
         public Time(int hours, int minutes, int seconds, int milliseconds)
         {
-            if (hours < 0 || hours > 23)
-                throw new ArgumentException("Hours must be between 0 and 23");
-
-            if (minutes < 0 || minutes > 59)
-                throw new ArgumentException("Minutes must be between 0 and 59");
-
-            if (seconds < 0 || seconds > 59)
-                throw new ArgumentException("Seconds must be between 0 and 59");
-
-            if (milliseconds < 0 || milliseconds > 999)
-                throw new ArgumentException("Milliseconds must be between 0 and 999");
+            ValidHour(hours);
+            ValidMinute(minutes);
+            ValidSecond(seconds);
+            ValidMillisecond(milliseconds);
 
             _hours = hours;
             _minutes = minutes;
@@ -87,12 +123,8 @@
 
         public bool IsOtherDay(Time other)
         {
-            int totalHours = _hours + other._hours;
-
-            if (totalHours > 23)
-                return true;
-
-            return false;
+            int totalMilliseconds = this.ToMilliseconds() + other.ToMilliseconds();
+            return totalMilliseconds >= 24 * 60 * 60 * 1000;
         }
 
         public Time Add(Time other)
@@ -102,28 +134,24 @@
             int m = _minutes + other._minutes;
             int h = _hours + other._hours;
 
-            // Ajustar milisegundos
             if (ms > 999)
             {
                 s += ms / 1000;
                 ms = ms % 1000;
             }
 
-            // Ajustar segundos
             if (s > 59)
             {
                 m += s / 60;
                 s = s % 60;
             }
 
-            // Ajustar minutos
             if (m > 59)
             {
                 h += m / 60;
                 m = m % 60;
             }
 
-            // Ajustar horas
             if (h > 23)
             {
                 h = h % 24;
---
+++ OOP01/01Fronted/Main.cs
@@ -0,0 +1,51 @@
+﻿using _01Bakends;
+using System;
+
+namespace _01Frontend
+{
+    internal class Main
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Time t1 = new Time();
+                Time t2 = new Time(10);
+                Time t3 = new Time(23, 58, 34, 666);
+                Time t4 = new Time(5, 10, 20, 300);
+                Time t5 = new Time(12, 30, 15);
+
+                Console.WriteLine("Horas originales:");
+                Console.WriteLine($"t1: {t1}");
+                Console.WriteLine($"t2: {t2}");
+                Console.WriteLine($"t3: {t3}");
+                Console.WriteLine($"t4: {t4}");
+                Console.WriteLine($"t5: {t5}");
+
+                Console.WriteLine("\nSumando cada hora con t3:");
+
+                Time[] times = { t1, t2, t3, t4, t5 };
+
+                foreach (Time t in times)
+                {
+                    Time result = t.Add(t3);
+                    Console.WriteLine($"{t} + {t3} = {result}");
+                }
+
+                Console.WriteLine("\nVerificando si pasa al siguiente día con t4:");
+
+                foreach (Time t in times)
+                {
+                    bool otherDay = t.IsOtherDay(t4);
+                    Console.WriteLine($"{t} + {t4} pasa al otro día? {otherDay}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
