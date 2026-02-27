4a0c2972419386f003e87d088072d26f4e17ff0e
Merxado <mayimamiyyo1112@gmail.com>
2026-02-27 12:39:30 -0500

    Add class Time

---
+++ OOP01/01Bakends/Time.cs
@@ -0,0 +1,79 @@
+﻿namespace _01Bakends
+{
+    public class Time
+    {
+        // Fields
+        private int _hours;
+        private int _minutes;
+        private int _seconds;
+        private int _milliseconds;
+
+        // Constructors
+        public Time() : this(0, 0, 0, 0)
+        {
+        }
+
+        public Time(int hours) : this(hours, 0, 0, 0)
+        {
+        }
+
+        public Time(int hours, int minutes) : this(hours, minutes, 0, 0)    
+        {
+        }
+
+        public Time(int hours, int minutes, int seconds) : this(hours, minutes, seconds, 0)
+        {
+        }
+
+        public Time(int hours, int minutes, int seconds, int milliseconds)
+        {
+            if (hours < 0 || hours > 23)
+                throw new ArgumentException("Hours must be between 0 and 23");
+
+            if (minutes < 0 || minutes > 59)
+                throw new ArgumentException("Minutes must be between 0 and 59");
+
+            if (seconds < 0 || seconds > 59)
+                throw new ArgumentException("Seconds must be between 0 and 59");
+
+            if (milliseconds < 0 || milliseconds > 999)
+                throw new ArgumentException("Milliseconds must be between 0 and 999");
+
+            _hours = hours;
+            _minutes = minutes;
+            _seconds = seconds;
+            _milliseconds = milliseconds;
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            return "";
+        }
+
+        public int ToMilliseconds()
+        {
+            return 0;
+        }
+
+        public int ToSeconds()
+        {
+            return 0;
+        }
+
+        public int ToMinutes()
+        {
+            return 0;
+        }
+
+        public bool IsOtherDay(Time other)
+        {
+            return false;
+        }
+
+        public Time Add(Time other)
+        {
+            return null;
+        }
+    }
+}
---
+++ OOP01/01Fronted/Program.cs
@@ -0,0 +1,11 @@
+﻿using _01Bakends;
+using System.Timers;
+
+try
+{
+    Time t = new Time(25, 10, 10, 10);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
